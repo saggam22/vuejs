9124112d26c0d97659338f3f73ed12d6d49014d6
saggam22 <saggam92@icloud.com>
2022-10-05 16:51:29 +0900

    single file component

---
+++ vue-form/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      //event.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      //http 통신 라이브러리 axios 브라우저 -> 서버로 데이터를 보내기 위함
+      //post는 데이터를 생성하거나 생성할때 사용하는 http 메소드
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
